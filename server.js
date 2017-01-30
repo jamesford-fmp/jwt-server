600d99715a21ce1123953dd1fe7d1344cfcd2887
James Ford <jford@findmypast.com>
2017-01-30 17:28:32 +0000

    Added an endpoint to generate auth token and set to cookie. Built a protected endpoint which outputs contents of the signed token

---
+++ server.js
@@ -0,0 +1,37 @@
+var jwt = require('jsonwebtoken');
+var express = require('express');
+var express_jwt = require('express-jwt');
+var cookieParser = require('cookie-parser')
+
+var app = express();
+
+var port = 7171;
+
+app.use(cookieParser());
+
+app.use(express_jwt({
+  secret: 'my-secret',
+  credentialsRequired: false,
+  getToken: (req) => {
+    return req.cookies.jwt_token;
+  }
+}));
+
+app.get('/protected',
+  (req, res) => {
+    if(!req.user) {
+      console.log("No auth token, redirecting");
+      res.redirect('/jwt');
+    }
+    if(!req.user.admin) return res.sendStatus(401);
+    res.json(req.user);
+  });
+
+app.get('/jwt', (req, res) => {
+  let token = jwt.sign({name: 'James Ford', admin: true}, 'my-secret');
+  res.cookie('jwt_token', token, {httpOnly: true});
+  res.send(token);
+});
+
+app.listen(port);
+console.log("Listening on port " + port);
